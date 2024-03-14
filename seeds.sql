123523ec86e257d52aebdcd5137f0fd1edde06b2
510rrl <rrlopez@uci.edu>
2024-03-14 14:36:31 -0700

    added values for seeds

--- seeds.sql
+++ seeds.sql
@@ -1,3 +1,8 @@
 INSERT INTO department (employee_names)
 VALUES ("Professor"),
         ("Engineer"),
+
+INSERT INTO employee_db (name, department)
+VALUES ("Role"),
+        ("Position"),
+        ("Height", 3)
