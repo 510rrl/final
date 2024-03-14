ce06ced523799baacb6b10115286cbf8061f1409
510rrl <rrlopez@uci.edu>
2024-03-14 14:17:19 -0700

    updated schema with ex 25 from activitiy

--- schema.sql
+++ schema.sql
@@ -4,7 +4,12 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE employee (
-    id 
+    id INT NOT NULL 
+    name VARCHAR(99) NOT NULL,
+    department INT,
+    FOREIGN KEY (employee)
+    REF
     active
-    
+
 );
+\\got this example from activitiy 25
